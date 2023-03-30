78df92051676705caf53a21dc4a38a2d155bcf3d
luisnavedag <luisjosenavedagarcia@gmail.com>
2023-03-30 00:21:08 -0300

    fix username error

--- user/models.py
+++ user/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+    username = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
 
--- user/views.py
+++ user/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserForm
+from .models import User
 # Create your views here.
 
 def register(request):
     form = UserForm()
+
     if request.method == 'POST':
         form = UserForm(request.POST)
 
         if form.is_valid():
+            
+
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
-            #login(request, user)
+            # login(request, user)
+
             return redirect('user-dashboard')
         else:
             print('fail to register')
-            messages.error(request, 'Fail! too easy password')
+            for field, errors in form.errors.items():
+                    for error in errors:
+                        print(f"Erro no campo {field}: {error}")
+            # messages.error(request, 'Fail! too easy password')
 
     context = {'form': form}
     return render(request, "user/register.html", context)
