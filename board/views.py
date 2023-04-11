4ae9ef2a65ae670062d7fcdf696e906e4c7867b5
luisnavedag <luisjosenavedagarcia@gmail.com>
2023-04-11 00:04:19 -0300

    Js posting data to django views

--- board/views.py
+++ board/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
-# Create your views here.
+
+#@csrf_exempt
 @login_required(login_url='login')
 def create_project(request):
     if request.method == 'POST':
         print('--------',request.POST)
+        return HttpResponse('ok')
     else:
         print('nothing -----------')
+        return HttpResponse('not ok')
--- static/js/project.js
+++ static/js/project.js
@@ -1,38 +1,21 @@
-/*
-<button onclick="basket_add(this)" id="{{ item.id }}"></button>
 
-
-  function send_productId(id){
+function createNewProject(projectName){
     var request = new XMLHttpRequest();
-    request.open('POST', '{% url 'basket:basket_add' %}', true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    const data = {
-      id: id,
-      csrfmiddlewaretoken:'{{ csrf_token }}',
-      action: "POST",
-    };
-    request.send(data);
-  };
 
-  function basket_add(obj){
-    send_productId(obj.id);
-  };
-
-*/
-
-function createNewProject(userId, projectName){
-    var request = new XMLHttpRequest();
     var myDiv = document.getElementById('createProjectEndpoint');
     var endpoint = myDiv.getAttribute('data-endpoint');
+
+    var csrfToken = document.getElementById('csrfToken').getAttribute('data-token');
+    
+    var userId = document.getElementById('userId').getAttribute('data-user');
     
     request.open('POST', endpoint, true);
-    request.getResponseHeader('Content-Type', 'applicantion/x-www-form-urlencoded; charset=UTF-8');
-    const data = {
-        id:userId,
-        projectName: projectName,
-        csrfmiddlewaretoken:'{{ csrf_token }}',
-        action: "POST"
-    };
+
+    const data = new FormData();
+    data.append('id', userId);
+    data.append('projectName', projectName);
+    data.append('csrfmiddlewaretoken', csrfToken);
+    data.append('action', 'POST');
 
     request.send(data)
 
